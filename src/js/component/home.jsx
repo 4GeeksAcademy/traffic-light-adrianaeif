e5820807142320cb5dacd376e321b2ae0af16e33
adrianaeif <adrianaisea02@gmail.com>
2024-08-06 03:29:24 +0000

    botón de agregar luz

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,28 +8,33 @@ const Home = () => {
 
 	const [color, setColor] = useState("");
 
+	const [luces, setLuces] = useState([{ nombre: "rojo", valor: "red" }, { nombre: "amarillo", valor: "yellow" }, { nombre: "verde", valor: "green" }])
+
 	return (
 
 		<div className="container mt-5">
 			<div className="container mt-5 bg-black" style={{ width: 200, borderRadius: '15%' }}>
 				<div className="d-inline-block d-flex flex-column align-items-center">
 					<button
-						className={"d-inline-block bg-danger rounded-circle m-3 " + (color == 'red' ? 'rojo' : '')}
-						style={{ height: 80, width: 80 }}
+						className={"d-inline-block rounded-circle m-3 " + (color == 'red' ? 'rojo' : '')}
+						style={{ background: faro.valor, height: 80, width: 80 }}
 						onClick={() => setColor("red")}
 					></button>
-					<button
-						className={"d-inline-block bg-warning rounded-circle m-3 " + (color == 'yellow' ? 'amarillo' : '')}
-						style={{ height: 80, width: 80 }}
-						onClick={() => setColor("yellow")}
-					></button>
-					<button
-						className={"d-inline-block bg-success rounded-circle m-3 " + (color == 'green' ? 'verde' : '')}
-						style={{ height: 80, width: 80 }}
-						onClick={() => setColor("green")}
-					></button>
+					{
+						luces.map(faro =>
+							<button
+								className={"d-inline-block bg-danger rounded-circle m-3 " + (color == faro.valor ? faro.nombre : '')}
+								style={{ height: 80, width: 80 }}
+								onClick={() => setColor(faro.valor)}
+							></button>)
+					}
 				</div>
 			</div>
+			<buttom className="btn btn-success" onClick={() => {
+				if (luces.length <= 3) {
+					setLuces([...luces, { nombre: "morado", valor: "purple" }])
+				}
+			}}>Agregar luz</buttom>
 		</div>
 	);
 }

d48c385a27e08d1eada5e1220f619a534b42b543
adrianaeif <adrianaisea02@gmail.com>
2024-08-08 17:14:42 +0000

    cambiar luces

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,43 +1,48 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 
-	const [color, setColor] = useState("");
+	const [color , setColor] = useState("");
 
-	const [luces, setLuces] = useState([{ nombre: "rojo", valor: "red" }, { nombre: "amarillo", valor: "yellow" }, { nombre: "verde", valor: "green" }])
+	const [luces, setLuces] = useState([ {nombre:'rojo' , valor:'red'}, {nombre:'amarillo' , valor:'yellow'}, {nombre:'verde' , valor:'green'}]);
+
+	const CambioColor = ()=>{
+        const color = ["red", "yellow", "green", "purple"];
+        const cambioColores = color[Math.floor(Math.random()*color.length)];
+        setColor(cambioColores);
+    }
+   
 
 	return (
+		
+		// semáforo
+		<div className="container mt-5 "> 
+			<div className="d-flex justify-content-center container mt-5 bg-black" style={{width: 200 , borderRadius: '15%'}}>
 
-		<div className="container mt-5">
-			<div className="container mt-5 bg-black" style={{ width: 200, borderRadius: '15%' }}>
-				<div className="d-inline-block d-flex flex-column align-items-center">
-					<button
-						className={"d-inline-block rounded-circle m-3 " + (color == 'red' ? 'rojo' : '')}
-						style={{ background: faro.valor, height: 80, width: 80 }}
-						onClick={() => setColor("red")}
-					></button>
+				<div className="d-inline-block d-flex flex-column align-item-center">
 					{
-						luces.map(faro =>
-							<button
-								className={"d-inline-block bg-danger rounded-circle m-3 " + (color == faro.valor ? faro.nombre : '')}
-								style={{ height: 80, width: 80 }}
-								onClick={() => setColor(faro.valor)}
-							></button>)
+						luces.map(faro=>
+						<button className={" d-inline-block rounded-circle m-3 " + (color == faro.valor ? faro.nombre : '') }  
+												style={{backgroundColor:faro.valor, height:80 , width: 80}}
+												onClick={()=>{setColor(faro.valor)
+						}}/>)
 					}
 				</div>
 			</div>
-			<buttom className="btn btn-success" onClick={() => {
-				if (luces.length <= 3) {
-					setLuces([...luces, { nombre: "morado", valor: "purple" }])
-				}
-			}}>Agregar luz</buttom>
+
+			{/* botenes para agregar y cambiar luces */}
+
+					<button className="btn btn-success" onClick={()=>{
+						if (luces.length <= 3) {
+							setLuces([...luces, {nombre:"morado", valor:"purple"}])
+						}
+					}}>Agregar luz</button>
+
+
+					<button className="btn btn-warning m-3" onClick={() => setColor(CambioColor)}>Cambiar luz</button>
 		</div>
 	);
-}
-
+};
 
 export default Home;
